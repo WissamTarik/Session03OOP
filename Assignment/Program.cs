4c9c811cf81dc9a0cf4b35c263dada9ed55339d7
WissamTarik <www.wissamtarik2021@gmail.com>
2025-07-25 18:53:37 +0300

    Part01 Q1

---
+++ Assignment/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part01
+            #region Q1
+            //b) To define a blueprint for a class
+            #endregion
+            #endregion
+        }
+    }
+}
