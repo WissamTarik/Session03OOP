9ac8637a669f38262d9eeecc4aed0347581dbc42
WissamTarik <www.wissamtarik2021@gmail.com>
2025-07-25 17:36:53 +0300

    Video02

---
+++ Session03OOP/Interface Ex01/IMyType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session03OOP.Interface_Ex01
+{
+    internal interface IMyType
+    {
+        //Default access modifer for interface is public
+        //What we can write inside the interface?
+        /*
+         1.Signature of properties
+         2.Signature of methods[Name-Parameters-Return Type]
+         3.Default implemented methods[Fully implemented method]
+         */
+
+        //1.Signature of properties
+        double Salary { get; set; }
+
+        //2.Signature of method
+        void Fun01();
+
+        //3.Default implemented methods[Fully implemented method]
+        //We can use private access modifer with only default implemented method
+
+        void Print()
+        {
+            Console.WriteLine("Default implemented methods[Fully implemented method]");
+        }
+
+    }
+}
--- Session03OOP/Program.cs
+++ Session03OOP/Program.cs
@@ -14,6 +14,28 @@
             //1.Static binding ->New
             //2.Dynamic binding ->override
             #endregion
+
+            #region Video02 Interface EX01
+            //Interface is a REFERENCE TYPE
+            // Is a code contract between the developer who write it and the developer
+            //   who use it   [implementation]
+
+            //IMyType myType=new IMyType();
+            //NOTE:U can't create an object from interface
+
+            //MyType myType=new MyType();
+            //myType.Salary = 12000;
+            //Console.WriteLine(myType.Salary);
+            //myType.Fun01();
+
+            //IMyType myType;
+            ////Reference->Can refer to any object of class implement the interface 'IMyType'
+            //myType = new MyType();
+            //myType.Salary = 1200;
+            //Console.WriteLine(myType);
+            //myType.Fun01();
+            //myType.Print();
+            #endregion
         }
     }
 }
